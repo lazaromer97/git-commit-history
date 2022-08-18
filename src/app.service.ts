935584657d3b0110879a2421fb3785c876d8dc0f
Lazaro Estrada <lazaromer97@gmail.com>
2022-08-18 03:18:23 -0400

    Create service for obtain commit history.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  async getCommitHistory() {
+    const result = await this.httpService
+      .get(
+        'https://api.github.com/repos/lazaromer97/git-commit-history/commits',
+      )
+      .toPromise();
+    return result.data || [];
   }
 }
